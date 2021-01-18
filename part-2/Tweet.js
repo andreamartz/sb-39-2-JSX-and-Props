5b74381714413a4d50687cdc24138b3d096b2840
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-18 16:11:43 -0600

    Finish Part 2

---
+++ part-2/Tweet.js
@@ -0,0 +1,8 @@
+const Tweet = (props) => (
+  <p>
+    <b>Username:</b> {props.username}
+    <b>Name:</b> {props.name}
+    <b>Date:</b> {props.date}
+    <b>Message:</b> {props.message}
+  </p>
+)
